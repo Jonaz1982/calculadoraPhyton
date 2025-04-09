a4a11a1850ad94b2b001e69fa54bb0fa94d540e3
Johnatan González Vélez <ingegonzalez1982@gmail.com>
2025-04-09 13:04:29 -0500

    adaptando main y variables

--- iac/main.tf
+++ iac/main.tf
@@ -31,4 +31,8 @@ module "monitoring" {
   location            = var.location
   resource_group_name = var.resource_group_name
   aks_clusters        = [module.aks_cluster_1.cluster_name, module.aks_cluster_2.cluster_name]
-}
+
+  k8s_host                   = module.aks.k8s_host
+    k8s_client_certificate     = module.aks.k8s_client_certificate
+    k8s_client_key             = module.aks.k8s_client_key
+    k8s_cluster_ca_certificate = module.aks.k8s_cluster_ca_certificate}
--- iac/modules/monitoring/variables.tf
+++ iac/modules/monitoring/variables.tf
@@ -9,3 +9,8 @@ variable "resource_group_name" {
 variable "aks_clusters" {
   type = list(string)
 }
+
+variable "k8s_host" {}
+variable "k8s_client_certificate" {}
+variable "k8s_client_key" {}
+variable "k8s_cluster_ca_certificate" {}
