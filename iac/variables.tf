eee08ef3ddf2d8300767c98263f3af8f755f854e
Johnatan González Vélez <ingegonzalez1982@gmail.com>
2025-04-10 17:23:42 -0500

    actualizando variables

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,25 +1,44 @@
 variable "location" {
+  description = "Azure region"
   type        = string
-  description = "Ubicación de los recursos"
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g., dev, prod)"
+  type        = string
 }
 
 variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
-  description = "Nombre del grupo de recursos"
 }
 
-variable "sql_admin_user" {
-  type        = string
-  description = "Usuario administrador de SQL Server"
-}
-
-variable "sql_admin_password" {
-  type        = string
-  description = "Contraseña del administrador de SQL Server"
-  sensitive   = true
-}
-
-variable "private_subnet_cidrs" {
+variable "vnet_address_space" {
+  description = "Address space for the virtual network"
   type        = list(string)
-  description = "CIDRs para las subredes privadas"
+}
+
+variable "aks_name_1" {
+  description = "Name of the first AKS cluster"
+  type        = string
+}
+
+variable "aks_name_2" {
+  description = "Name of the second AKS cluster"
+  type        = string
+}
+
+variable "node_vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+}
+
+variable "node_min_count" {
+  description = "Minimum node count for AKS autoscaling"
+  type        = number
+}
+
+variable "node_max_count" {
+  description = "Maximum node count for AKS autoscaling"
+  type        = number
 }
