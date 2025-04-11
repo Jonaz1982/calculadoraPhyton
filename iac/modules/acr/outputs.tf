849cf1884e44fb63135c146f18a39a1d1c53e8dc
Johnatan González Vélez <ingegonzalez1982@gmail.com>
2025-04-11 12:02:07 -0500

    eliminado duplicidad

--- iac/modules/acr/outputs.tf
+++ iac/modules/acr/outputs.tf
@@ -10,7 +10,3 @@ output "acr_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
 }
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
